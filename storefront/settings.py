f075da8853134558d6fa3e4238fd26f6a4caeb93
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-07 14:54:09 +0500

    Installing Django Restframework

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framwork',
     'debug_toolbar',
     'playground',
     'store',
