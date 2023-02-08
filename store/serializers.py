4d9b367ba640969df3620cfd393fd41f0efb2047
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-08 14:27:28 +0500

    Build the Collections API

--- store/serializers.py
+++ store/serializers.py
@@ -6,7 +6,9 @@ from decimal import Decimal
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'products_count']
+
+    products_count = serializers.IntegerField()
     # id = serializers.IntegerField()
     # title = serializers.CharField(max_length=255)
 
--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('products/', views.product_list),
     path('products/<int:id>/', views.product_details),
+    path('collections/', views.collection_list),
     path('collections/<int:pk>/', views.collection_detail,name="collection-detail")
 
 ]
