02fa9c82313c0151a4d19ae4c498da5ae7b05d77
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-10 19:52:07 +0500

    Extending the User Model

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.models import Product
 from store.admin import ProductAdmin
 from tags.models import TagItem
+from .models import User
 
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2","email","first_name","last_name"),
+            },
+        ),
+    )
+
+
 class TagInline(GenericTabularInline):
     autocomplete_fields = ['tag']
     min_num = 1
@@ -18,4 +33,4 @@ class CustomProductAdmin(ProductAdmin):
 
 
 admin.site.unregister(Product)
-admin.site.register(Product,CustomProductAdmin)
+admin.site.register(Product, CustomProductAdmin)
--- storefront/settings.py
+++ storefront/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'store',
     'tags',
     'likes',
-    'store_custom'
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +136,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+AUTH_USER_MODEL = 'core.User'
