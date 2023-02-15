8a5ff7dc6eebfe121eb05ce4728e411acc9f76f4
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-15 18:30:11 +0500

    Fixtures

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,53 +1,73 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
+from model_bakery import baker
 from rest_framework import status
 import pytest
+from store.models import Collection, Product
+
+
+
+
+@pytest.fixture
+def create_collection(api_client):
+    def do_create_collection(collection):
+        return api_client.post('/store/collections/', collection)
+
+    return do_create_collection
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_returns_401(self):
+    def test_if_user_is_anonymous_returns_401(self, api_client, create_collection):
         # AAA (Arrange,Act,Assert)
 
         # Arrange
 
         # Act
 
-        client = APIClient()
-        response = client.post('/store/collections/', {'title': 'a'})
+        response = create_collection({'title': 'a'})
 
         # Assert
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_returns_403(self):
-        client = APIClient()
-        client.force_authenticate(user={})
-        response = client.post('/store/collections/', {'title': 'a'})
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        authenticate(is_staff=False)
+        response = create_collection({'title': 'a'})
 
         # Assert
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self):
-
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': ''})
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': ''})
 
         # Assert
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_returns_201(self):
-
-        client = APIClient()
-        client.force_authenticate(user=User(is_staff=True))
-        response = client.post('/store/collections/', {'title': 'a'})
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        authenticate(is_staff=True)
+        response = create_collection({'title': 'a'})
 
         # Assert
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
 
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
