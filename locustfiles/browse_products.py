9fd7c7eb025eaf8bebe0fc21f47aad7c56e2f09f
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-15 19:47:34 +0500

    Running Performance tests with locust

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -7,16 +7,19 @@ class WebsiteUser(HttpUser):
 
     @task(2)
     def view_products(self):
+        print('View Products')
         collection_id = randint(2, 6)
         self.client.get(f'/store/products/?collection_id={collection_id}', name='/store/products')
 
     @task(4)
     def view_product(self):
+        print('View Product Details')
         product_id = randint(1, 1000)
         self.client.get(f'/store/products/{product_id}', name='/store/products/:id')
 
     @task(1)
     def add_to_cart(self):
+        print('Add to Cart')
         product_id = randint(1, 10)
         self.client.post(
             f'/store/carts/{self.cart_id}',
